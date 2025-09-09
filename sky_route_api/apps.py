13b8105768587b585686329f45d6499618c2be80
turulko-oleksandr <sashaturulko@i.ua>
2025-09-09 14:42:20 +0300

    init: create starter projects and add gitignore

---
+++ sky_route_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SkyRouteApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sky_route_api'
---
+++ sky_route_api/urls.py
@@ -0,0 +1,5 @@
+app_name = 'sky_route_api'
+
+urlpatterns = [
+
+]
