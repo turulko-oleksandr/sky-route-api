31a9ac73fb59a31a3088732c9cf5953401dfc2ff
turulko-oleksandr <sashaturulko@i.ua>
2025-09-09 17:37:53 +0300

    feat(user): add authentication and user auth-endpoints

--- sky_route_api_service/settings.py
+++ sky_route_api_service/settings.py
@@ -126,3 +126,16 @@ STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'accounts.User'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "10/day", "user": "30/day"},
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
