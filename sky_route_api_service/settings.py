cd92eb224b24fe5fac6c8ec40628bd63fb774b0d
turulko-oleksandr <sashaturulko@i.ua>
2025-09-16 11:06:04 +0300

    feat(requirements): specify all used libraries in requirements.txt and update jwt lifetime from 1 day to 1 hour

--- sky_route_api_service/settings.py
+++ sky_route_api_service/settings.py
@@ -160,7 +160,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5, days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
